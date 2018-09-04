cfac71a9ddf2ce8356f2279e7858ed82201ec7df
215401925 <215401925@qq.com>
2018-09-04 09:57:46 +0800

    Signed-off-by: 215401925 <215401925@qq.com>

--- Public/joyshang/myjavascript/zscan_index.js
+++ Public/joyshang/myjavascript/zscan_index.js
@@ -2,13 +2,22 @@
   * 2018/7/26.
  */
 //JavaScript代码区域
-layui.use(['jquery','element','table','laytpl','form'], function(){
+layui.use(['jquery','element','table','laytpl','form','laydate'], function(){
     var element = layui.element
         ,table=layui.table
         ,laytpl=layui.laytpl
         ,form=layui.form
+        ,laydate = layui.laydate
         ,$=layui.$;
 
+	  //日期
+	  laydate.render({
+	    elem: '#date'
+	  });
+	  laydate.render({
+	    elem: '#date1'
+	  });
+	  
     //表格实例
     table.render({
        elem: '#demo'
@@ -26,7 +35,7 @@ layui.use(['jquery','element','table','laytpl','form'], function(){
     });
 
 
-    //监听POST提交
+    //监听POST提交-分配订单
     form.on('submit(btn_dingdanhao)', function(up_data){
         //alert($("#dingdanhao").name);
         //layer.msg($("#dingdanhao1").value);
@@ -82,7 +91,43 @@ layui.use(['jquery','element','table','laytpl','form'], function(){
 
 
 
-
+    //监听POST提交-查询订单
+    form.on('submit(btn_inquire)', function(up_data){
+    	
+        $.ajax({
+            type: 'POST',
+            url: "dd_check",
+            data: up_data.field,
+            dataType: 'json',
+            success: function (get_data) {
+                console.log(get_data);
+                //return false;
+                table.render({
+                    elem: '#table_ps'
+                    ,height: 615
+                    ,url:'rend_table_ps'
+                    ,page:true
+                    ,even: true //开启隔行背景
+                    ,size: 'sm' //小尺寸的表格
+                    ,initSort: {
+                        field: 'id' //排序字段，对应 cols 设定的各字段名
+                        ,type: 'desc' //排序方式  asc: 升序、desc: 降序、null: 默认排序
+                    }
+                    ,cols:[[
+                        {field:'id',title:'ID',width:50,sort:true}
+                        ,{field:'dingdanhao',title:'订单号',width:180}
+                        ,{field:'chepaihao',title:'车牌号',width:80}
+                        ,{field:'shoujianren',title:'收件人',width:80}
+                        ,{field:'sddizhi',title:'地址',width:220}
+                        ,{field:'paisongyuan',title:'派送员',width:80}
+                        ,{field:'paisongtime',title:'扫描时间',width:150}
+                        ,{field:'paisongstatus',title:'派送状态',width:120}
+                    ]]
+                });
+            }
+        });
+        return false;
+    });
 
     //监听单元格编辑
     table.on('edit(main_table)', function(obj){
@@ -97,8 +142,6 @@ layui.use(['jquery','element','table','laytpl','form'], function(){
 $(function () {
     $("")
 });
-
-
 
 $(function () {
     $("#btn_test").click(function(){
